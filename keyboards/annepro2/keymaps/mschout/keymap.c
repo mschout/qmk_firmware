c97ef7566f99739f57b32aaee86ee053d25b07e9
Michael Schout <schoutm@gmail.com>
2020-09-17 19:16:21 -0500

    Switch to space cadet macros for parens, add more media keys

--- keyboards/annepro2/keymaps/mschout/keymap.c
+++ keyboards/annepro2/keymaps/mschout/keymap.c
@@ -16,31 +16,10 @@ enum anne_pro_layers {
 
 
 enum td_keycodes {
-    TD_ALT_LP,
-    TD_ALT_RP,
-    TD_ESC_CAPS
+  TD_ESC_CAPS
 };
 
-// Define a type containing as many tapdance states as you need
-typedef enum {
-    SINGLE_TAP,
-    SINGLE_HOLD,
-    DOUBLE_SINGLE_TAP
-} td_state_t;
-
-// Create a global instance of the tapdance state type
-static td_state_t td_state;
-
-// tapdance functions
-uint8_t cur_dance(qk_tap_dance_state_t *state);
-void altlp_finished(qk_tap_dance_state_t *state, void *user_data);
-void altlp_reset(qk_tap_dance_state_t *state, void *user_data);
-void altrp_finished(qk_tap_dance_state_t *state, void *user_data);
-void altrp_reset(qk_tap_dance_state_t *state, void *user_data);
-
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_ALT_LP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, altlp_finished, altlp_reset),
-  [TD_ALT_RP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, altrp_finished, altrp_reset),
   [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 };
 
@@ -76,7 +55,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     KC_TAB         , KC_Q   , KC_W         , KC_E  , KC_R         , KC_T         , KC_Y    , KC_U         , KC_I    , KC_O           , KC_P   , KC_LBRC      , KC_RBRC, KC_BSLS,
     TD(TD_ESC_CAPS), KC_A   , KC_S         , KC_D  , KC_F         , KC_G         , KC_H    , KC_J         , KC_K    , KC_L           , KC_SCLN, KC_QUOT      , KC_ENT ,
     KC_LSFT        , KC_Z   , KC_X         , KC_C  , KC_V         , KC_B         , KC_N    , KC_M         , KC_COMM , KC_DOT         , KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL        , KC_LGUI, TD(TD_ALT_LP), KC_SPC, TD(TD_ALT_RP), LT(_FN1_LAYER, KC_LEFT), LT(_FN2_LAYER, KC_DOWN), RCTL_T(KC_RGHT)
+    KC_LCPO        , KC_LGUI, KC_RALT      , KC_SPC, KC_LALT      , LT(_FN1_LAYER, KC_LEFT), LT(_FN2_LAYER, KC_DOWN), KC_RCPC
 ),
   /*
   * Layer _FN1_LAYER
@@ -117,8 +96,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   */
  [_FN2_LAYER] = KEYMAP( /* Base */
     ________      , KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, ________      , ________      , ________, KC_AP_LED_OFF, KC_AP_LED_ON, ________, ________   , ________ , ________,
-    MO(_FN2_LAYER), ________  , KC_UP     , ________  , ________  , ________      , ________      , ________, ________     , ________    , KC_PSCR , KC__VOLDOWN, KC__VOLUP, ________,
-    ________      , KC_LEFT   , KC_DOWN   , KC_RGHT   , ________  , ________      , ________      , ________, ________     , ________    , KC_PGUP , KC_PGDN    , ________ ,
+    MO(_FN2_LAYER), ________  , KC_UP     , ________  , ________  , ________      , ________      , ________, ________     , ________    , KC_PSCR , KC__VOLDOWN, KC__VOLUP, KC__MUTE,
+    ________      , KC_LEFT   , KC_DOWN   , KC_RGHT   , ________  , ________      , ________      , ________, ________     , ________    , KC_PGUP , KC_PGDN    , KC_MPLY  ,
     ________      , ________  , ________  , ________  , ________  , ________      , ________      , ________, ________     , KC_INS      , KC_DEL  , ________   ,
     ________      , ________  , ________  , ________  , ________  , MO(_FN1_LAYER), MO(_FN2_LAYER), ________
  ),
@@ -136,78 +115,4 @@ void matrix_scan_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t layer) {
     return layer;
-}
-
-// Function to determine the current tapdance state
-uint8_t cur_dance(qk_tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (state->interrupted || !state->pressed)
-      return SINGLE_TAP;
-    else
-      return SINGLE_HOLD;
-  }
-
-  if (state->count == 2)
-    return DOUBLE_SINGLE_TAP;
-  else
-    return 3;
-}
-
-// `finished` and `reset` functions for each tapdance keycode
-void altlp_finished(qk_tap_dance_state_t *state, void *user_data) {
-  td_state = cur_dance(state);
-
-  switch (td_state) {
-    case SINGLE_TAP:
-      register_code16(KC_LEFT_PAREN);
-      break;
-    case SINGLE_HOLD:
-      register_mods(MOD_BIT(KC_LALT));
-      break;
-    case DOUBLE_SINGLE_TAP:
-      tap_code16(KC_LEFT_PAREN);
-      register_code16(KC_LEFT_PAREN);
-  }
-}
-
-void altlp_reset(qk_tap_dance_state_t *state, void *user_data) {
-  switch (td_state) {
-    case SINGLE_TAP:
-      unregister_code16(KC_LEFT_PAREN);
-      break;
-    case SINGLE_HOLD:
-      unregister_mods(MOD_BIT(KC_LALT));
-      break;
-    case DOUBLE_SINGLE_TAP:
-      unregister_code16(KC_LEFT_PAREN);
-  }
-}
-
-void altrp_finished(qk_tap_dance_state_t *state, void *user_data) {
-  td_state = cur_dance(state);
-
-  switch (td_state) {
-    case SINGLE_TAP:
-      register_code16(KC_RIGHT_PAREN);
-      break;
-    case SINGLE_HOLD:
-      register_mods(MOD_BIT(KC_RALT));
-      break;
-    case DOUBLE_SINGLE_TAP:
-      tap_code16(KC_RIGHT_PAREN);
-      register_code16(KC_RIGHT_PAREN);
-  }
-}
-
-void altrp_reset(qk_tap_dance_state_t *state, void *user_data) {
-  switch (td_state) {
-    case SINGLE_TAP:
-      unregister_code16(KC_RIGHT_PAREN);
-      break;
-    case SINGLE_HOLD:
-      unregister_mods(MOD_BIT(KC_RALT));
-      break;
-    case DOUBLE_SINGLE_TAP:
-      unregister_code16(KC_RIGHT_PAREN);
-  }
 }
