04bc66ff92dda56cac99fda22ee3d6404a62058a
Michael Schout <schoutm@gmail.com>
2020-09-17 19:47:43 -0500

    Spacecadet: put braces and brackets on ctrl and alt

---
+++ users/mschout/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define TAPPING_TERM 200
+
+// SpaceCadet CTRL keys send [] on tap
+#define LCPO_KEYS KC_LCTRL, KC_LSFT, KC_LBRACKET
+#define RCPC_KEYS KC_RCTRL, KC_RSFT, KC_RBRACKET
+
+// SpaceCadet ALT keys send {} on tap
+#define LAPO_KEYS KC_LALT, KC_TRNS, KC_LBRACKET
+#define RAPC_KEYS KC_RALT, KC_TRNS, KC_RBRACKET
+
